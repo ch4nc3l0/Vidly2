27d1b957c3cac5eb1e52589adbd0c3851c17c412
chance <ch4nc3l0@gmail.com>
2019-12-15 12:49:21 -0500

    Add project files.

---
+++ Vidly2/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult CustomerDetails(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+               new Customer { Id = 1, Name = "Chance"},
+               new Customer { Id = 2, Name = "Tamara"}
+            };
+        }
+    }
+}
---
+++ Vidly2/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name ="The Ring"},
+                new Movie { Id = 2, Name="Shrek"},
+                new Movie { Id = 3, Name="Breaking Bad"}
+            };
+        }
+    }
+}
