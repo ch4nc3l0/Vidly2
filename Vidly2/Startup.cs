6434a8b2ec3794d24b6a099deebff63dd57f8e0c
chance <ch4nc3l0@gmail.com>
2019-12-15 13:05:45 -0500

    Move Navbar to partial view

--- Vidly2/Startup.cs
+++ Vidly2/Startup.cs
@@ -62,7 +62,7 @@ namespace Vidly2
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Customer}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
